3fd1fbc952b8375678420ff6d61cc012b8b155ba
YongHun Byun <yonghunbyun@gmail.com>
2018-05-25 23:59:38 +0900

    Use SSO_USERNAME_FIELD env variable

--- config/sso.php
+++ config/sso.php
@@ -6,5 +6,5 @@ return [
     'server_endpoint' => env('SSO_SERVER_ENDPOINT', null),
     'broker_id' => env('SSO_BROKER_ID', null),
     'broker_secret' => env('SSO_BROKER_SECRET', null),
-    'username_field' => 'email',
+    'username_field' => env('SSO_USERNAME_FIELD', 'email'),
 ];
